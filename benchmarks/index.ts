1198fb6001454fb851204b783c1862ab0fc66840
Mikhail Nasyrov <streambot@gmail.com>
2023-12-22 00:04:05 +0300

    chore: Custom "build" script for publishing fine-graned package

--- benchmarks/index.ts
+++ benchmarks/index.ts
@@ -1,7 +1,7 @@
 import * as REF_CORE from 'nrgy-reference';
 import { Bench } from 'tinybench';
 
-import * as DEV_CORE from '../dist/core';
+import * as DEV_CORE from '../dist';
 import { createLatch } from '../src/utils/latch';
 
 const ITERATION_COUNT = 200;
---
+++ build.ts
@@ -0,0 +1,60 @@
+import fs from 'node:fs';
+import tsup from 'tsup';
+
+import pkg from './package.json';
+
+const entries = [
+  // Core libs
+  { entry: 'index', source: 'src/core' },
+  { entry: 'mvc', source: 'src/core/mvc' },
+  { entry: 'store', source: 'src/core/store' },
+
+  // Extensions
+  { entry: 'ditox', source: 'src/ditox' },
+  { entry: 'ditox-react', source: 'src/ditox-react' },
+  { entry: 'react', source: 'src/react' },
+  { entry: 'rx-effects', source: 'src/rx-effects' },
+  { entry: 'rxjs', source: 'src/rxjs' },
+  { entry: 'rxjs-react', source: 'src/rxjs-react' },
+];
+
+main();
+
+async function main() {
+  // Compile
+  await compile();
+
+  const compiledFiles = fs.readdirSync('dist');
+
+  // Copy package files
+  console.log('Copy files to the dist folder...');
+
+  pkg.files
+    .filter((name) => name !== 'dist')
+    .map((localPath) => {
+      console.log(`- ${localPath} ...`);
+
+      fs.cpSync(localPath, `dist/${localPath}`, { recursive: true });
+    });
+
+  // Write public package.json
+  pkg.private = false;
+  pkg.files = [...new Set([...pkg.files, ...compiledFiles])];
+  pkg.scripts = undefined as any;
+  pkg.devDependencies = undefined as any;
+
+  fs.writeFileSync(`dist/package.json`, JSON.stringify(pkg, undefined, 2));
+}
+
+async function compile() {
+  await tsup.build({
+    entry: entries.reduce(
+      (obj, { entry, source }) => ({ ...obj, [entry]: `${source}/index.ts` }),
+      {},
+    ),
+    clean: true,
+    dts: true,
+    format: ['cjs', 'esm'],
+    external: Object.keys(pkg.optionalDependencies),
+  });
+}
