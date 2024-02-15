cc13e246073d593606617bbdba0f66dd190f9e06
Mikhail Nasyrov <streambot@gmail.com>
2024-02-15 23:29:00 +0300

    test: Added unit tests

--- src/core/atom.test.ts
+++ src/core/atom.test.ts
@@ -1,6 +1,32 @@
 import { collectChanges, flushMicrotasks } from '../test/testUtils';
 
-import { atom } from './atom';
+import { atom, getAtomNode, isAtom } from './atom';
+import { compute } from './compute';
+import { syncEffect } from './effect';
+import { getSignalNode } from './signal';
+
+describe('isAtom()', () => {
+  it('should return "true" in case a value is Atom', () => {
+    expect(isAtom(atom(1))).toBe(true);
+    expect(isAtom(atom(1).asReadonly())).toBe(true);
+    expect(isAtom(compute(() => 1))).toBe(true);
+
+    expect(isAtom(undefined)).toBe(false);
+    expect(isAtom(null)).toBe(false);
+    expect(isAtom(1)).toBe(false);
+    expect(isAtom('2')).toBe(false);
+    expect(isAtom(NaN)).toBe(false);
+    expect(isAtom({})).toBe(false);
+  });
+});
+
+describe('getAtomNode()', () => {
+  it('should return an internal AtomNode of the atom', () => {
+    expect(getAtomNode(atom(1))).toBeDefined();
+    expect(getAtomNode(atom(1).asReadonly())).toBeDefined();
+    expect(getAtomNode(compute(() => 1))).toBeDefined();
+  });
+});
 
 describe('atom()', () => {
   it('should create a value store with the provided initial state', () => {
@@ -35,6 +61,11 @@ describe('Atom', () => {
       const store = atom({ value: 1 });
       store.set({ value: 2 });
       expect(store()).toEqual({ value: 2 });
+
+      // Do not update destroyed atom
+      store.destroy();
+      store.set({ value: 3 });
+      expect(store()).toEqual({ value: 2 });
     });
 
     it('must use a custom comparator', async () => {
@@ -56,6 +87,25 @@ describe('Atom', () => {
         { value: 2, data: 'c' },
       ]);
     });
+
+    it('should not notify destroyed effect', () => {
+      const onChange = jest.fn();
+
+      const store = atom<number>(1);
+      const fx = syncEffect(store, onChange);
+
+      onChange.mockReset();
+      store.set(2);
+      expect(onChange).toHaveBeenCalledTimes(1);
+      expect(onChange).toHaveBeenCalledWith(2);
+
+      onChange.mockReset();
+      fx.destroy();
+      store.set(3);
+      const node = getSignalNode(fx.onDestroy);
+      expect(node.isDestroyed).toBe(true);
+      expect(onChange).toHaveBeenCalledTimes(0);
+    });
   });
 
   describe('update()', () => {
@@ -74,6 +124,27 @@ describe('Atom', () => {
       store.destroy();
 
       expect(await statePromise).toEqual([{ value: 1 }]);
+    });
+  });
+
+  describe('mutate()', () => {
+    it('should provide the current value to the mutator function and trigger "has changed" flow without any equality checks', () => {
+      const store = atom({ value: 1 });
+
+      const collectedValues: number[] = [];
+      syncEffect(store, (state) => collectedValues.push(state.value));
+
+      store.mutate((state) => (state.value = 2));
+      store.mutate((state) => (state.value = 3));
+      store.mutate(() => {
+        // Do nothing
+      });
+
+      // Do not mutate destroyed atom
+      store.destroy();
+      store.mutate((state) => (state.value = 4));
+
+      expect(collectedValues).toEqual([1, 2, 3, 3]);
     });
   });
 
@@ -98,6 +169,34 @@ describe('Atom', () => {
       const store = atom<number>(1, { onDestroy });
 
       store.destroy();
+      expect(onDestroy).toHaveBeenCalledTimes(1);
+
+      // It should destroy once if called twice
+      store.destroy();
+      expect(onDestroy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not notify destroyed effects', () => {
+      const onDestroy = jest.fn();
+
+      const store = atom<number>(1);
+      const fx = syncEffect(store, () => {});
+      syncEffect(fx.onDestroy, onDestroy);
+
+      store.set(2);
+      fx.destroy();
+
+      const node = getSignalNode(fx.onDestroy);
+
+      expect(node.isDestroyed).toBe(true);
+      expect(onDestroy).toHaveBeenCalledTimes(1);
+
+      store.destroy();
+      expect(node.isDestroyed).toBe(true);
+      expect(onDestroy).toHaveBeenCalledTimes(1);
+
+      store.destroy();
+      expect(node.isDestroyed).toBe(true);
       expect(onDestroy).toHaveBeenCalledTimes(1);
     });
   });
