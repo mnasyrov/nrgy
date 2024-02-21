29defd7166f55b3224a6402b197a935a07306ef3
Mikhail Nasyrov <streambot@gmail.com>
2024-02-21 23:49:47 +0300

    test: Added tests for SignalEffect

---
+++ src/core/signalEffect.test.ts
@@ -0,0 +1,132 @@
+import { syncEffect } from './effect';
+import {
+  createMicrotaskScheduler,
+  createSyncTaskScheduler,
+} from './schedulers';
+import { getSignalNode } from './signal';
+import { SignalEffect } from './signalEffect';
+
+describe('SignalEffect', () => {
+  describe('destroy()', () => {
+    it('should destroy the effect', () => {
+      const effect = new SignalEffect(createSyncTaskScheduler(), () => {});
+      effect.destroy();
+      expect(effect.isDestroyed).toBe(true);
+    });
+
+    it('should call onDestroy signal', () => {
+      const onDestroyCallback = jest.fn();
+
+      const effect = new SignalEffect(createSyncTaskScheduler(), () => {});
+      syncEffect(effect.onDestroy, onDestroyCallback);
+
+      effect.destroy();
+      expect(onDestroyCallback).toHaveBeenCalledTimes(1);
+    });
+
+    it('should destroy onDestroy, onError, onResult signals', () => {
+      const effect = new SignalEffect(createSyncTaskScheduler(), () => {});
+
+      effect.destroy();
+
+      expect(getSignalNode(effect.onResult).isDestroyed).toBe(true);
+      expect(getSignalNode(effect.onError).isDestroyed).toBe(true);
+      expect(getSignalNode(effect.onDestroy).isDestroyed).toBe(true);
+    });
+  });
+
+  describe('notify()', () => {
+    it('should notify the effect', () => {
+      const scheduler = createMicrotaskScheduler();
+      const action = jest.fn();
+
+      const effect = new SignalEffect(scheduler, action);
+
+      effect.notify(1);
+      expect(action).toHaveBeenCalledTimes(0);
+
+      expect(scheduler.isEmpty()).toBe(false);
+      scheduler.execute();
+      expect(scheduler.isEmpty()).toBe(true);
+
+      expect(action).toHaveBeenCalledTimes(1);
+      expect(action).toHaveBeenCalledWith(1);
+    });
+
+    it('should not notify the effect if it is destroyed', () => {
+      const scheduler = createSyncTaskScheduler();
+      const action = jest.fn();
+
+      const effect = new SignalEffect(scheduler, action);
+      effect.destroy();
+
+      effect.notify(1);
+      expect(action).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  describe('notifyDestroy()', () => {
+    it('should notify the effect that it must be destroyed', () => {
+      const effect = new SignalEffect(createSyncTaskScheduler(), () => {});
+
+      effect.notifyDestroy();
+
+      expect(effect.isDestroyed).toBe(true);
+    });
+
+    it('should not notify the effect if it is destroyed', () => {
+      const effect = new SignalEffect(createSyncTaskScheduler(), () => {});
+
+      const mockDestroy = jest.fn(effect.destroy.bind(effect));
+      effect.destroy = mockDestroy;
+
+      effect.notifyDestroy();
+      expect(mockDestroy).toHaveBeenCalledTimes(1);
+
+      mockDestroy.mockClear();
+      effect.notifyDestroy();
+      expect(mockDestroy).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  describe('run()', () => {
+    it('should signals onResult with result of action', () => {
+      const resultCallback = jest.fn();
+      const scheduler = createSyncTaskScheduler();
+
+      const effect = new SignalEffect(scheduler, () => 'result');
+      syncEffect(effect.onResult, resultCallback);
+
+      effect.notify(undefined);
+
+      expect(resultCallback).toHaveBeenCalledTimes(1);
+      expect(resultCallback).toHaveBeenCalledWith('result');
+    });
+
+    it('should signals onError with error of action', () => {
+      const errorCallback = jest.fn();
+      const scheduler = createSyncTaskScheduler();
+
+      const effect = new SignalEffect(scheduler, () => {
+        throw new Error('error');
+      });
+      syncEffect(effect.onError, errorCallback);
+
+      effect.notify(undefined);
+
+      expect(errorCallback).toHaveBeenCalledTimes(1);
+      expect(errorCallback).toHaveBeenCalledWith(new Error('error'));
+    });
+
+    it('should not run action if the effect is destroyed', () => {
+      const action = jest.fn();
+      const scheduler = createSyncTaskScheduler();
+
+      const effect = new SignalEffect(scheduler, action);
+      effect.destroy();
+
+      effect.run(undefined);
+      expect(action).toHaveBeenCalledTimes(0);
+    });
+  });
+});
--- src/core/signalEffect.ts
+++ src/core/signalEffect.ts
@@ -3,27 +3,44 @@ import { TaskScheduler } from './schedulers';
 import { destroySignal, signal } from './signal';
 import { createWeakRef } from './utils/createWeakRef';
 
+/**
+ * SignalEffect represents a subscription to a signal
+ */
 export class SignalEffect<T> implements SignalEffectNode<any> {
   readonly ref: WeakRef<SignalEffectNode<T>> = createWeakRef(this);
 
   isDestroyed = false;
 
+  /**
+   * Signals a result of the action function
+   */
   readonly onResult = signal<any>();
+
+  /**
+   * Signals an error which occurred in the execution of the action function
+   */
   readonly onError = signal<unknown>({ sync: true });
+
+  /**
+   * Signals that the effect has been destroyed
+   */
   readonly onDestroy = signal<void>({ sync: true });
 
   private scheduler?: TaskScheduler;
-  private callback?: (value: T) => unknown;
+  private action?: (value: T) => unknown;
 
-  constructor(scheduler: TaskScheduler, callback: (value: T) => unknown) {
+  constructor(scheduler: TaskScheduler, action: (value: T) => unknown) {
     this.scheduler = scheduler;
-    this.callback = callback;
+    this.action = action;
   }
 
+  /**
+   * Destroys the effect
+   */
   destroy(): void {
     this.isDestroyed = true;
     this.scheduler = undefined;
-    this.callback = undefined;
+    this.action = undefined;
 
     this.onDestroy();
 
@@ -32,6 +49,9 @@ export class SignalEffect<T> implements SignalEffectNode<any> {
     destroySignal(this.onDestroy);
   }
 
+  /**
+   * Schedule the effect to be run
+   */
   notify(value: T): void {
     if (this.isDestroyed) {
       return;
@@ -40,6 +60,9 @@ export class SignalEffect<T> implements SignalEffectNode<any> {
     this.scheduler?.schedule(() => this.run(value));
   }
 
+  /**
+   * Notify the effect that it must be destroyed
+   */
   notifyDestroy(): void {
     if (this.isDestroyed) {
       return;
@@ -48,13 +71,19 @@ export class SignalEffect<T> implements SignalEffectNode<any> {
     this.destroy();
   }
 
+  /**
+   * Execute the subscriber's action in the context of this effect.
+   *
+   * Should be called by the user scheduling algorithm when the provided
+   * `scheduler` TaskScheduler is called.
+   */
   run(value: T): void {
-    if (this.isDestroyed || !this.callback) {
+    if (this.isDestroyed || !this.action) {
       return;
     }
 
     try {
-      const result = this.callback(value);
+      const result = this.action(value);
       this.onResult(result);
     } catch (error) {
       this.onError(error);
