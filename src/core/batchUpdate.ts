6f50146135e6597d755a8e707c0124f791e8fa72
Mikhail Nasyrov <streambot@gmail.com>
2024-04-09 01:29:36 +0300

    docs: Minor docs update

--- src/core/batchUpdate.ts
+++ src/core/batchUpdate.ts
@@ -1,5 +1,10 @@
 import { ENERGY_RUNTIME } from './runtime';
 
+/**
+ * Run a function in a batch update context.
+ *
+ * It will defer all effect notifications until the function is finished.
+ */
 export function batchUpdate(action: () => any): void {
   ENERGY_RUNTIME.batchUpdate(action);
 }
